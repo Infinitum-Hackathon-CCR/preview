0a5887b57ec9edbae362e30413a793972bc0ec27
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-16 20:05:20 -0300

    Mudanças nos Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ class Card extends Component {
     const { thumbnail, title, body } = this.props;
     return (
       <div className="card">
-        <img src={thumbnail} alt={title}/>
+        <img className="card-thumbnail" src={thumbnail} alt={title}/>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p>{body}</p>
