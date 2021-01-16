ab4f23e355f9af7b06068d357e6423ef1bbd178d
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-16 16:07:18 -0300

    Commit inicial

---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
+
+class Home extends Component {
+  render() { 
+    return (
+      <main>
+        <Header />
+        <Footer />
+      </main>
+    );
+  }
+}
+ 
+export default Home;
